06116d56953faa74e1b8e7125282f27625d1ca73
Yogesh Singh <yogesh.singh.2810@gmail.com>
2018-12-04 18:08:01 +0530

    added new test app

---
+++ Extension App/ContentExtension/NotificationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationViewController.swift
+//  ContentExtension
+//
+//  Created by Yogesh Singh on 04/12/18.
+//  Copyright © 2018 Yogesh Singh. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
---
+++ Extension App/WEGExtensionApp/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  WEGExtensionApp
+//
+//  Created by Yogesh Singh on 04/12/18.
+//  Copyright © 2018 Yogesh Singh. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
+
