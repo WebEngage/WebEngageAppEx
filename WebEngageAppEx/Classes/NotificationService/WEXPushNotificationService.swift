28d476ff2afd5bc42a3af752bca3529658e28de3
Yogesh Singh <yogesh.singh.2810@gmail.com>
2019-07-08 19:19:47 +0530

    added Notification Service Swift file

---
+++ WebEngageAppEx/Classes/NotificationService/WEXPushNotificationService.swift
@@ -0,0 +1,146 @@
+//
+//  WEXPushNotificationService.swift
+//  WEGExtensionApp
+//
+//  Created by Yogesh Singh on 08/07/19.
+//  Copyright © 2019 Yogesh Singh. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+@available(iOS 10.0, *)
+class WEXPushNotificationService: UNNotificationServiceExtension {
+
+    var contentHandler: ((UNNotificationContent) -> Void)?
+    var bestAttemptContent: UNMutableNotificationContent?
+
+    override func didReceive(_ request: UNNotificationRequest,
+                             withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+
+        self.contentHandler = contentHandler
+        self.bestAttemptContent = request.content.mutableCopy() as? UNMutableNotificationContent
+
+        print("Push Notification Content: \(request.content.userInfo)")
+
+        guard let details = request.content.userInfo["expandableDetails"] as? [AnyHashable: Any] else {
+            return deliverContent()
+        }
+
+        guard let style = details["style"] as? String else {
+            return deliverContent()
+        }
+
+        switch style {
+        case "CAROUSEL_V1":
+            loadCarouselView(with: details)
+        case "RATING_V1":
+            loadRatingView(with: details)
+        case "BIG_PICTURE":
+            loadRatingView(with: details)
+        default:
+            deliverContent()
+        }
+    }
+
+    override func serviceExtensionTimeWillExpire() {
+        deliverContent()
+    }
+}
+
+@available(iOS 10.0, *)
+extension WEXPushNotificationService {
+
+    /// Delivers the currently available modified notification content on contentHandler
+    func deliverContent() {
+        if let handler = contentHandler, let content = bestAttemptContent {
+            handler(content)
+        }
+    }
+
+    func loadCarouselView(with details: [AnyHashable: Any]) {
+
+        guard let items = details["items"] as? [[AnyHashable: Any]] else {
+            return deliverContent()
+        }
+
+        guard items.count >= 3  else {
+            return deliverContent()
+        }
+
+        var itemsCounter = 0
+        var imageCounter = 0
+        var attachmentsArray: [UNNotificationAttachment] = []
+
+        for item in items {
+            if let imageStr = item["image"] as? String {
+                if let imageURL = URL(string: imageStr) {
+
+                    loadAttachment(for: imageURL, at: itemsCounter) { (attachment) in
+
+                        imageCounter += 1
+
+                        if let mediaAttachment = attachment {
+                            attachmentsArray.append(mediaAttachment)
+                            self.bestAttemptContent?.attachments = attachmentsArray
+                        }
+
+                        if imageCounter == items.count {
+                            self.deliverContent()
+                        }
+                    }
+
+                    itemsCounter += 1
+                }
+            }
+        }
+    }
+
+    func loadRatingView(with details: [AnyHashable: Any]) {
+
+        if let imageStr = details["image"] as? String {
+            if let imageURL = URL(string: imageStr) {
+                loadAttachment(for: imageURL, at: 0) { (attachment) in
+                    if let mediaAttachment = attachment {
+                        self.bestAttemptContent?.attachments = [mediaAttachment]
+                    }
+
+                    self.deliverContent()
+                }
+            }
+        }
+    }
+
+    func loadAttachment(for imageURL: URL,
+                        at index: Int,
+                        completion handler: @escaping (UNNotificationAttachment?) -> Void) {
+
+        let pathExtension = imageURL.pathExtension
+
+        URLSession.shared.downloadTask(with: imageURL) { (temporaryLocation, _, error) in
+
+            var attachment: UNNotificationAttachment?
+
+            if let error = error {
+                print("error: \(error)")
+            } else {
+                if let temporaryLocation = temporaryLocation {
+                    let localURL = URL(fileURLWithPath: temporaryLocation.path.appending(pathExtension))
+
+                    do {
+
+                        try FileManager.default.moveItem(at: temporaryLocation, to: localURL)
+
+                        attachment = try UNNotificationAttachment(identifier: "\(index)", url: localURL, options: nil)
+
+                    } catch let error {
+                        print("error: \(error)")
+                    }
+                }
+            }
+
+            handler(attachment)
+
+        }.resume()
+    }
+}
