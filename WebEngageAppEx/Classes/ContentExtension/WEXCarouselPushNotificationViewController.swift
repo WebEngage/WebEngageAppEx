7fac9261b95e917810bab103a00561670869a5a3
Yogesh Singh <yogesh.singh@webklipper.com>
2019-12-30 15:53:34 +0530

    added carousel push vc

--- WebEngageAppEx/Classes/ContentExtension/WEXCarouselPushNotificationViewController.swift
+++ WebEngageAppEx/Classes/ContentExtension/WEXCarouselPushNotificationViewController.swift
@@ -10,6 +10,46 @@ import UIKit
 
 class WEXCarouselPushNotificationViewController: WEXRichPushLayout {
     
+    var current = 0
+    var isRendering = false
+    var carouselItems: [Any]?
+    var images: [String]?
+    var wasLoaded: [String]?
     
-
+//    var notification: UNNotification?
+//    var userDefaults: UserDefaults?
+//    var currentLayout: WEXRichPushLayout?
+    
+    // MARK: - Content Extension Delegates
+    
+    override func didReceive(_ notification: UNNotification) {
+        
+        isRendering = true
+        current = 0
+        
+        if setupCarouselItems(from: notification) {
+        
+            let downloadedCount = notification.request.content.attachments.count
+            
+        }
+    }
+    
+    
+    // MARK: - View Helpers
+    
+    func setupCarouselItems(from notification: UNNotification) -> Bool {
+     
+        if let expandableDetails = notification.request.content.userInfo["expandableDetails"] as? [AnyHashable: Any] {
+            
+            if let items = expandableDetails["items"] as? [Any] {
+                
+                if items.count > 0 {
+                    carouselItems = items
+                    return true
+                }
+            }
+        }
+        
+        return false
+    }
 }
