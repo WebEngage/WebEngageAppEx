b4654ceb062f9238b8a68a77289e08ad7001f98b
Uday Sharma <Uday.sharma@webengage.com>
2023-11-22 14:09:21 +0530

    code updated for push overlay.

--- WebEngageAppEx/Classes/ContentExtension/WEXBannerPushNotificationViewController.m
+++ WebEngageAppEx/Classes/ContentExtension/WEXBannerPushNotificationViewController.m
@@ -70,9 +70,7 @@ API_AVAILABLE(ios(10.0))
                 if ([attachment.URL startAccessingSecurityScopedResource]) {
                     NSData *imageData = [NSData dataWithContentsOfFile:attachment.URL.path];
                     UIImage *image = [UIImage imageWithData:imageData];
-                    
                     [attachment.URL stopAccessingSecurityScopedResource];
-                    
                     if (image) {
                         imageView.image = image;
                     } else {
@@ -91,8 +89,89 @@ API_AVAILABLE(ios(10.0))
     
     imageView.contentMode = UIViewContentModeScaleAspectFill;
     [mainContentView addSubview:imageView];
+    [self setUpAppIconWithName];
+}
+
+- (void)setUpAppIconWithName {
+    UIView *superViewWrapper = self.view.subviews[0];
+    UIView *mainContentView = superViewWrapper.subviews[0];
+
+    // Create a superview for the app icon and app name
+    UIView *appInfoContainer = [[UIView alloc] init];
+    [mainContentView addSubview:appInfoContainer];
+
+    // Assuming you have access to the app icon and app name
+    UIImage *appIcon = [self appIcon];
+
+    UIImageView *appIconImageView = [[UIImageView alloc] initWithImage:appIcon];
+    appIconImageView.contentMode = UIViewContentModeScaleAspectFit;
+    [appInfoContainer addSubview:appIconImageView];
+
+    UILabel *appNameLabel = [[UILabel alloc] init];
+    NSString* appName = [self appName];
+    appNameLabel.text = appName;
+    appNameLabel.font = [UIFont boldSystemFontOfSize:15.0]; // Set your desired bold font size
+    appNameLabel.textColor = [UIColor blackColor]; // Set your desired text color
+    appNameLabel.textAlignment = NSTextAlignmentLeft;
+    
+    // Set up a shadow with a vibrant color
+    appNameLabel.layer.shadowColor = [UIColor whiteColor].CGColor; // Shadow color
+    appNameLabel.layer.shadowRadius = 5.0; // Spread of the shadow
+    appNameLabel.layer.shadowOpacity = 1.0; // Opacity of the shadow
+    appNameLabel.layer.shadowOffset = CGSizeZero; // Offset of the shadow
+
+    // Set up the attributed text with increased thickness
+    NSDictionary *attributes = @{
+        NSStrokeWidthAttributeName: @-5.0, // Set your desired thickness value
+        NSStrokeColorAttributeName: [UIColor blackColor] // Set your desired text color
+    };
+    appNameLabel.attributedText = [[NSAttributedString alloc] initWithString:appName attributes:attributes];
+
+    [appInfoContainer addSubview:appNameLabel];
+
+
+    // Set up constraints for app icon and app name within the appInfoContainer
+    appIconImageView.translatesAutoresizingMaskIntoConstraints = NO;
+    appNameLabel.translatesAutoresizingMaskIntoConstraints = NO;
+
+    [appIconImageView.leadingAnchor constraintEqualToAnchor:appInfoContainer.leadingAnchor].active = YES;
+    [appIconImageView.centerYAnchor constraintEqualToAnchor:appInfoContainer.centerYAnchor].active = YES;
+    [appIconImageView.widthAnchor constraintEqualToConstant:20.0].active = YES; // Set your desired width
+    [appIconImageView.heightAnchor constraintEqualToConstant:20.0].active = YES; // Set your desired height
+
+    [appNameLabel.leadingAnchor constraintEqualToAnchor:appIconImageView.trailingAnchor constant:10.0].active = YES;
+    [appNameLabel.centerYAnchor constraintEqualToAnchor:appIconImageView.centerYAnchor].active = YES;
+
+    // Add the appInfoContainer to the mainContentView
+    appInfoContainer.translatesAutoresizingMaskIntoConstraints = NO;
+    [appInfoContainer.leadingAnchor constraintEqualToAnchor:mainContentView.leadingAnchor constant:10.0].active = YES;
+    [appInfoContainer.heightAnchor constraintEqualToConstant:20.0].active = YES; // Set your desired height
+    [appInfoContainer.topAnchor constraintEqualToAnchor:mainContentView.topAnchor constant:10].active = YES;
+
     [self setupLabelsContainer];
 }
+
+
+
+- (UIImage *) appIcon {
+    NSUserDefaults *sharedDefaults = [self getSharedUserDefaults];
+    // Retrieve NSData from UserDefaults
+       NSData *appIconData = [sharedDefaults objectForKey:@"app_icon"];
+       
+       // Convert NSData back to UIImage
+       if (appIconData) {
+           UIImage *appIcon = [UIImage imageWithData:appIconData];
+           return appIcon;
+       } else {
+           return nil; // Handle the case where the image data is not available
+       }
+}
+
+- (NSString *) appName {
+    NSUserDefaults *sharedDefaults = [self getSharedUserDefaults];
+    return [sharedDefaults objectForKey:@"app_name"];
+}
+
 
 - (void)setupLabelsContainer {
     UIView *superViewWrapper = self.view.subviews[0];
@@ -141,23 +220,25 @@ API_AVAILABLE(ios(10.0))
     [richContentView addSubview:richTitleLabel];
     [richContentView addSubview:richSubLabel];
     [richContentView addSubview:richBodyLabel];
-    
     [superViewWrapper addSubview:richContentView];
     
     [self setupConstraints];
+    
 }
 
 - (void)setupConstraints {
     UIView *superViewWrapper = self.view.subviews[0];
     UIView *mainContentView = superViewWrapper.subviews[0];
     UIView *richContentView = superViewWrapper.subviews[1];
+    UIView *appIconView = mainContentView.subviews[1];
+    
     
     if (@available(iOS 10.0, *)) {
         superViewWrapper.translatesAutoresizingMaskIntoConstraints = NO;
         [superViewWrapper.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor].active = YES;
         [superViewWrapper.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor].active = YES;
         [superViewWrapper.topAnchor constraintEqualToAnchor:self.view.topAnchor].active = YES;
-        [superViewWrapper.bottomAnchor constraintEqualToAnchor:richContentView.bottomAnchor].active = YES;
+        [appIconView.bottomAnchor constraintEqualToAnchor:richContentView.topAnchor].active = YES;
         
         //Top level view constraints
         mainContentView.translatesAutoresizingMaskIntoConstraints = NO;
@@ -168,7 +249,7 @@ API_AVAILABLE(ios(10.0))
         richContentView.translatesAutoresizingMaskIntoConstraints = NO;
         [richContentView.leadingAnchor constraintEqualToAnchor:richContentView.superview.leadingAnchor].active = YES;
         [richContentView.trailingAnchor constraintEqualToAnchor:richContentView.superview.trailingAnchor].active = YES;
-        [richContentView.topAnchor constraintEqualToAnchor:mainContentView.bottomAnchor].active = YES;
+        [richContentView.topAnchor constraintEqualToAnchor:appIconView.bottomAnchor].active = YES;
         
         [self.viewController.bottomLayoutGuide.topAnchor constraintEqualToAnchor:superViewWrapper.bottomAnchor].active = YES;
         
@@ -242,6 +323,31 @@ API_AVAILABLE(ios(10.0))
     } else {
         NSLog(@"Expected to be running iOS version 10 or above");
     }
+}
+
+- (NSUserDefaults *)getSharedUserDefaults {
+    
+    NSString *appGroup = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"WEX_APP_GROUP"];
+    
+    if (!appGroup) {
+        NSBundle *bundle = [NSBundle mainBundle];
+        
+        if ([[bundle.bundleURL pathExtension] isEqualToString:@"appex"]) {
+            bundle = [NSBundle bundleWithURL:[[bundle.bundleURL URLByDeletingLastPathComponent] URLByDeletingLastPathComponent]];
+        }
+        
+        NSString *bundleIdentifier = [bundle objectForInfoDictionaryKey:@"CFBundleIdentifier"];
+        
+        appGroup = [NSString stringWithFormat:@"group.%@.WEGNotificationGroup", bundleIdentifier];
+    }
+    
+    NSUserDefaults *defaults = [[NSUserDefaults alloc] initWithSuiteName:appGroup];
+    
+    if (!defaults) {
+        NSLog(@"Shared User Defaults could not be initialized. Ensure Shared App Groups have been enabled on Main App & Notification Service Extension Targets.");
+    }
+    
+    return defaults;
 }
 
 #endif
